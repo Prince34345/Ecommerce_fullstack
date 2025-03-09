ed036b46be01f608031bab8b03f6f890cc8aeb86
Prince34345 <priyanshudhawan55@gmail.com>
2025-03-09 17:23:28 +0530

    store modal added.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,13 @@
+"use client"
+import { Modal } from "@/components/ui/modal";
 import { UserButton, SignedIn } from "@clerk/nextjs";
 
 const SetupPage = () => {
     return (
      <div className="p-4">
-        <UserButton/>
+       <Modal title="test" description="text" isOpen onClose={() => {}}>
+           children
+       </Modal>
      </div>
     );
 }
