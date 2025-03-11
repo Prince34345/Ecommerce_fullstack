1dabff872ea8767aeb8e3f89ba9c181493c9b727
Prince34345 <priyanshudhawan55@gmail.com>
2025-03-11 11:35:11 +0530

    zod and form validation added.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,22 @@
 "use client"
 import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
 import { UserButton, SignedIn } from "@clerk/nextjs";
+import { useEffect } from "react";
 
 const SetupPage = () => {
+    const onOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen);
+
+    useEffect(() => {
+       if (!isOpen) {
+          onOpen();
+       }
+    }, [isOpen, onOpen])
+
     return (
      <div className="p-4">
-       <Modal title="test" description="text" isOpen onClose={() => {}}>
-           children
-       </Modal>
+       Root page
      </div>
     );
 }
