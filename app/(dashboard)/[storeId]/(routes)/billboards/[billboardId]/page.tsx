b912c1e51c6976657493b5ca0fd661d274d1820f
Prince34345 <priyanshudhawan55@gmail.com>
2025-05-16 21:23:56 +0530

    changes

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,21 +1,22 @@
 import prismadb from "@/lib/prismadb"
 import { BillBoardform } from "./components/billboard-form"
 
-interface BillboardPageProps { 
+interface PageProps {
   params: {
     storeId: string;
     billboardId: string;
-  }
+  };
+  searchParams?: { [key: string]: string | string[] | undefined };
 }
 
 const BillboardPage = async ({
   params
-}: BillboardPageProps) => {
+}: PageProps) => {
   const billboard = await prismadb.billboard.findUnique({
-      where: {
-          id: params.billboardId
-      }    
-  })
+    where: {
+        id: params.billboardId
+    } 
+})
   return (
      <div className="flex-col">
         <div className="flex-1 space-y-4 p-8 pt-6">
